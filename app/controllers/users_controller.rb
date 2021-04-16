ff49a4d516df1ba4ccb6713e1e90ddf94af6e9f3
danielufeli <danielufeli@yahoo.com>
2021-04-16 23:16:12 +0100

    Add user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to "/users/#{params[:id]}/edit"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
